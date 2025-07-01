5dfea1adef48a8508c151c3e23994835e9dc9342
Akash <Akash.patel.ap88020@gmail.com>
2025-07-01 15:25:40 +0530

    initial setup

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { assets } from '../assets/assets';
+
+const Header = () => {
+  return (
+    <div className='mx-8 sm:mx-16 xl:mx-24 relative overflow-hidden'>
+      
+      {/* Background image */}
+      <img 
+        src={assets.gradientBackground} 
+        alt="background" 
+        className='absolute top-1/2 left-1/2 w-full max-w-none -translate-x-1/2 -translate-y-1/2 -z-10 opacity-50' 
+      />
+
+      <div className='text-center mt-20 mb-8'>
+        <div className='inline-flex justify-center items-center px-6 py-1.5 gap-4 mb-4 border border-purple-400/40 bg-purple-400/40 text-sm text-purple-600 rounded-full '>
+          <p>New: AI feature integrated</p>
+          <img src={assets.star_icon} alt="star_icon" className='w-2.5' />
+        </div>
+        
+        <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-tight text-gray-700'>
+          Your own <span className='text-purple-500'>blogging</span> <br /> platform.
+        </h1>
+
+        <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs'>
+          Welcome to your personal blogging space.  
+          Share your thoughts, ideas, and stories with the world.  
+          Powered by modern design and AI-enhanced features.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+    const navigate = useNavigate();
+  return (
+    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
+        <img 
+            onClick={()=> navigate('/')} 
+            src={assets.logo}
+            alt="logo" 
+            className='w-3 sm:w-44 cursor-pointer'
+        />
+
+        <button
+         onClick={()=> navigate('/admin')}
+         className='bg-purple-400 text-white flex items-center px-6 py-2  rounded-full gap-2 cursor-pointer'>login
+            <img src={assets.arrow} alt="arrow" className='w-3' />
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
